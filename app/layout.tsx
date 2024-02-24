c3f0de21ccc098cbdb13cf10e6036a9b3aa95547
PawanK06 <pawan06kumar2003@gmail.com>
2024-02-24 23:52:00 +0530

    working on linear ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta 
+        name="viewport"
+        content="width=device-width, height=device-height, initial-scale=1"
+        />
+      </head>
+      <body className={inter.className}><div>
+        <Header/>
+        <main className="pt-navigation-height bg-page-gradient">{children}</main>
+        <Footer/>
+      </div></body>
     </html>
   );
 }
